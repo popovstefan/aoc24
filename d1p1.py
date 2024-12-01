ce2997290996c7d69b19a892156f69bf0d69157d
Stefan Popov <steffan.popov@protonmail.com>
2024-12-01 10:21:43 +0100

    Update d1p1.py

--- d1p1.py
+++ d1p1.py
@@ -20,4 +20,24 @@ def p1():
 
     solution = sum([abs(x[0] - x[1]) for x in zip(left ,right)])
     print(solution)
-    
+
+
+def p2():
+    left = []
+    right = []
+    for row in puzzle_input:
+        elements = row.split()
+        left.append(int(elements[0]))
+        right.append(int(elements[1]))
+
+    similarities = {}
+    for el in left:
+        if el not in similarities:
+            similarities[el] = (el * right.count(el))
+        else:
+            similarities[el] += (el * right.count(el))
+
+    solution = sum(similarities.values())
+    print(solution)
+
+p2()
