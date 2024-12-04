7138dc6587383f6e9d6e2206550d573ab4a4dcf4
Stefan Popov <steffan.popov@protonmail.com>
2024-12-04 15:32:39 +0100

    Update d4.py

--- d4.py
+++ d4.py
@@ -14,14 +14,14 @@ def p1(pi):
             # check main diagonal
             diag = "".join(np.diag(sw, 0))
             if diag == "XMAS" or diag[::-1] == "XMAS":
-                print(diag)
                 result += 1
             # check rows and columns
             for i in range(len(diag)):
+                if i > 0:
+                    break
                 row = "".join(sw[i, :])
                 column = "".join(sw[:, i])
                 if row == "XMAS" or row[::-1] == "XMAS":
-                    print(row)
                     result += 1
                 if column == "XMAS" or column[::-1] == "XMAS":
                     print(column)
@@ -29,19 +29,18 @@ def p1(pi):
             # check side diagonal
             diag = "".join(np.diag(np.fliplr(sw), 0))
             if diag == "XMAS" or diag[::-1] == "XMAS":
-                print(diag)
                 result += 1
         return result
 
     pi = [list(i + ' ' * (len(max(pi)) - len(i))) for i in pi]
 
+    # pad existing input
+    pi = np.array(pi)
+    pi = np.pad(array=pi, pad_width=((3, 3), (3, 3)), mode="constant", constant_values='Y')
     sliding_windows = np.lib.stride_tricks.sliding_window_view(x=pi, window_shape=(4, 4))
-    print(sliding_windows.shape)
     solution = 0
     for sliding_window_row in sliding_windows:
-        # todo counts same occurrence multiple times, try padding
         solution += count_occurrences(sliding_window_row)
-
     print(solution)
 
 
