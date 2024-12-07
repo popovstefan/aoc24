49fc859aa9ab2c460e0eba045b0cd5229f1b5462
Stefan Popov <steffan.popov@protonmail.com>
2024-12-07 15:39:22 +0100

    Update d7.py

--- d7.py
+++ d7.py
@@ -36,4 +36,38 @@ def p1():
     print(solution)
 
 
-p1()
+def p2():
+    solution = 0
+    for line in puzzle_input:
+        parts = line.split(":")
+        eq_res = int(parts[0])
+        eq = parts[1].split()
+        eq_numbers = []
+        for eq_el in eq:
+            eq_numbers.append(int(eq_el.strip()))
+        print(eq_res, eq_numbers)
+
+        combos = [''.join(map(str, i)) for i in product([0, 1, 2], repeat=len(eq_numbers) - 1)]
+        for combo in combos:
+            combo_res = eq_numbers[0]
+            n = len(combo)
+            i = 0
+            while i < n:
+                char = combo[i]
+                if char == '0':
+                    combo_res += eq_numbers[i + 1]
+                elif char == '1':
+                    combo_res *= eq_numbers[i + 1]
+                else:
+                    combo_res = int(str(combo_res) + str(eq_numbers[i + 1]))
+                i += 1
+                # if combo_res >= eq_res:
+                #     break
+            if combo_res == eq_res:
+                print("adding", combo_res)
+                solution += eq_res
+                break
+    print(solution)
+
+
+p2()
