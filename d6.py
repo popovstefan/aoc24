5133a25d9d7c34a256d2f6f8570f1afe52b3bf5b
Stefan Popov <steffan.popov@protonmail.com>
2024-12-08 18:12:36 +0100

    Update d6.py

--- d6.py
+++ d6.py
@@ -121,16 +121,17 @@ def p2():
     for inx in range(n):
         for jnx in range(m):
             if puzzle_input[inx][jnx] != clear and puzzle_input[inx][jnx] != wall:
-                guard = (inx, jnx)
+                guard = (inx, jnx, '^')
                 break
     print(guard)
     queue = collections.deque()
+    visited = []
     queue.append(guard)
     seen = {guard}
-    loops = {}
+    loops = 0
     while queue:
         path = queue.popleft()
-        x, y = path
+        x, y, direction = path
         # if the guard has left the maze, break away
         if x >= n or x < 0 or y < 0 or y >= m:
             break
@@ -138,65 +139,69 @@ def p2():
         try:
             while True:
                 if puzzle_input[x][y] == '^':  # top
+                    if (x - 1, y, go_right['^']) in seen:
+                        loops += 1
                     if puzzle_input[x - 1][y] == clear:
                         # clean current place
                         puzzle_input[x][y] = clear
                         # move the guard
                         puzzle_input[x - 1][y] = '^'
                         # mark it as seen
-                        if (x - 1, y) in seen:
-                            loops[(x, y)] = 1
-                        seen.add((x - 1, y))
+                        seen.add((x - 1, y, '^'))
                         # add the new place to the queue
-                        queue.append((x - 1, y))
+                        queue.append((x - 1, y, '^'))
+                        visited.append((x - 1, y, '^'))
                         print(x - 1, y)
                         break
                     else:
                         puzzle_input[x][y] = go_right[puzzle_input[x][y]]
                 if puzzle_input[x][y] == '>':  # right
+                    if (x, y + 1, go_right['>']) in seen:
+                        loops += 1
                     if puzzle_input[x][y + 1] == clear:
                         # clean current place
                         puzzle_input[x][y] = clear
                         # move the guard
                         puzzle_input[x][y + 1] = '>'
                         # mark it as seen
-                        if (x, y + 1) in seen:
-                            loops[(x, y)] = 1
-                        seen.add((x, y + 1))
+                        seen.add((x, y + 1, '>'))
                         # add the new place to the queue
-                        queue.append((x, y + 1))
+                        queue.append((x, y + 1, '>'))
+                        visited.append((x, y + 1, '>'))
                         print(x, y + 1)
                         break
                     else:
                         puzzle_input[x][y] = go_right[puzzle_input[x][y]]
                 if puzzle_input[x][y] == 'v':  # bottom
+                    if (x + 1, y, go_right['v']) in seen:
+                        loops += 1
                     if puzzle_input[x + 1][y] == clear:
                         # clean current place
                         puzzle_input[x][y] = clear
                         # move the guard
                         puzzle_input[x + 1][y] = 'v'
                         # mark it as seen
-                        if (x + 1, y) in seen:
-                            loops[(x, y)] = 1
-                        seen.add((x + 1, y))
+                        seen.add((x + 1, y, 'v'))
                         # add the new place to the queue
-                        queue.append((x + 1, y))
+                        queue.append((x + 1, y, 'v'))
+                        visited.append((x + 1, y, 'v'))
                         print(x + 1, y)
                         break
                     else:
                         puzzle_input[x][y] = go_right[puzzle_input[x][y]]
                 if puzzle_input[x][y] == '<':  # left
+                    if (x, y - 1, go_right['<']) in seen:
+                        loops += 1
                     if puzzle_input[x][y - 1] == clear:
                         # clean current place
                         puzzle_input[x][y] = clear
                         # move the guard
                         puzzle_input[x][y - 1] = '<'
                         # mark it as seen
-                        if (x, y - 1) in seen:
-                            loops[(x, y)] = 1
-                        seen.add((x, y - 1))
+                        seen.add((x, y - 1, '<'))
                         # add the new place to the queue
-                        queue.append((x, y - 1))
+                        queue.append((x, y - 1, '<'))
+                        visited.append((x, y - 1, '<'))
                         print(x, y - 1)
                         break
                     else:
@@ -204,9 +209,21 @@ def p2():
         except IndexError:
             # index out of bounds, anywhere
             print(len(seen))
-            print(len(loops))
+            print(loops)
+
     # print(len(seen) - 1)
-    # print(len(loops))
+
+    def is_cycle(apath):
+        return len(apath) != len(set(apath))
+
+    print(loops)
+    loops = 0
+    for i in range(len(visited)):
+        path = visited[:i]
+        # print(path)
+        if is_cycle(path):
+            loops += 1
+    print(loops)
 
 
 p2()
