f39a8b70af1d92fdaf304f26c6595e010c91a8e0
Stefan Popov <steffan.popov@protonmail.com>
2024-12-15 14:11:22 +0100

    Update d15.py

--- d15.py
+++ d15.py
@@ -7,20 +7,75 @@ pwd = os.path.dirname(__file__)
 # read input
 puzzle_input = read_puzzle_input(os.path.join(pwd, "puzzle_inputd15.txt"))
 
-grid = []
-movements = []
 
-for i, line in enumerate(puzzle_input):
-    if line.strip() == "":
-        continue
-    if line.startswith("#"):
-        grid.append([])
-        for char in line:
-            grid[i].append(char)
+def read_elements(p):
+    g = []
+    r = None
+    moves = []
+
+    for i, line in enumerate(p):
+        if line.strip() == "":
+            continue
+        if line.startswith("#"):
+            g.append([])
+            for j, char in enumerate(line):
+                if char == '@':
+                    r = (i, j)
+                g[i].append(char)
+        else:
+            for char in line:
+                moves.append(char)
+    return g, r, moves, len(g), len(g[0])
+
+
+grid, robot, movements, N, M = read_elements(puzzle_input)
+
+
+def can_move(mv, rbt, grd):
+    x, y = rbt
+    if mv == '^':
+        # check if there's at least one free cell top
+        for i in range(x):
+            if grd[i][y] == '.':
+                return True
+    elif mv == '>':
+        # check if there's at least one free cell right
+        for i in range(y, M):
+            if grd[x][i] == '.':
+                return True
+    elif mv == '<':
+        # check if there's at least one free cell left
+        for i in range(y):
+            if grd[x][i] == '.':
+                return True
+    elif mv == 'v':
+        # check if there's at least one free cell top
+        for i in range(x, N):
+            if grd[i][y] == '.':
+                return True
     else:
-        for char in line:
-            movements.append(char)
+        # for weird, unsupported movements
+        return False
+    # default, return no free cells
+    return False
 
 
-print(grid)
-print(movements)
+def move(mv, rbt, grd):
+    return rbt, grd
+
+
+def p1(robot_, grid_):
+    for itr, movement in enumerate(movements):
+        if can_move(movement, robot_, grid_):
+            print(itr, "moving", robot_)
+            robot_, grid_ = move(movement, robot_, grid_)
+
+    result = 0
+    for i in range(N):
+        for j in range(M):
+            if grid_[i][j] == 'O':
+                result += (i * 100 + j)
+    print(result)
+
+
+p1(robot, grid)
